dfd83ee34ac36bec67412e2153d67658920bcf2f
alexbrandon3 <alexlbrandon@gmail.com>
2021-08-25 13:52:14 -0400

    added functionality to JavaScript buttons and made minor CSS formatting adjustments

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,6 @@ var incorrectAnswers = 0;
 //Start button
 var startButton = document.querySelector("#start");
 function startTimer(){
-    console.log("clicked");
     var secondsLeft = 75;
     setInterval(function(){
         secondsLeft--;
@@ -21,6 +20,7 @@ function startTimer(){
 };
 startButton.addEventListener("click", startTimer);
 
+//Sets questions into an array
 var questions = [
     {
         questionText: "Commonly used data types DO NOT include:",
@@ -63,25 +63,132 @@ var questions = [
         correctAnswer: "console.log"
     }
 ];
-var questionContainer = document.querySelector(".questions");
-    var question = document.createElement("h2");
-    question.textContent = questions[currentIndex].questionText;
-    var answer1 = document.createElement('button');
-    answer1.textContent = questions[currentIndex].answer1;
-    answer1.setAttribute("data-value", questions[currentIndex].answer1);
-    var answer2 = document.createElement('button');
-    answer2.textContent = questions[currentIndex].answer2;
-    answer2.setAttribute("data-value", questions[currentIndex].answer2);
-    var answer3 = document.createElement('button');
-    answer3.textContent = questions[currentIndex].answer3;
-    answer3.setAttribute("data-value", questions[currentIndex].answer3);
-    var answer4 = document.createElement('button');
-    answer4.textContent = questions[currentIndex].answer4;
-    answer4.setAttribute("data-value", questions[currentIndex].answer4);
-    questionContainer.append(question);
-    questionContainer.append(answer1);
-    questionContainer.append(answer2);
-    questionContainer.append(answer3);
-    questionContainer.append(answer4);
 
-    //create function check value against questions current index
+//Displays questions dynamically
+var questionContainer = document.querySelector(".questions");
+var question = document.createElement("h2");
+question.textContent = questions[currentIndex].questionText;
+
+var answer1 = document.createElement('button');
+answer1.textContent = questions[currentIndex].answer1;
+answer1.setAttribute("data-value", questions[currentIndex].answer1);
+
+var answer2 = document.createElement('button');
+answer2.textContent = questions[currentIndex].answer2;
+answer2.setAttribute("data-value", questions[currentIndex].answer2);
+
+var answer3 = document.createElement('button');
+answer3.textContent = questions[currentIndex].answer3;
+answer3.setAttribute("data-value", questions[currentIndex].answer3);
+
+var answer4 = document.createElement('button');
+answer4.textContent = questions[currentIndex].answer4;
+answer4.setAttribute("data-value", questions[currentIndex].answer4);
+
+questionContainer.append(question);
+questionContainer.append(answer1);
+questionContainer.append(answer2);
+questionContainer.append(answer3);
+questionContainer.append(answer4);
+
+//Checks value against questions current index
+function checkAnswer1(){
+    var response = answer1.getAttribute("data-value");
+    var rightAnswer = questions[currentIndex].correctAnswer;
+    if(response === rightAnswer){
+        currentIndex++;
+        correctAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;
+    }
+    else{
+        secondsLeft -= 10;
+        timeDisplay.textContent = "Time: " + secondsLeft;
+        currentIndex++;
+        incorrectAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4; 
+    }
+}
+function checkAnswer2(){
+    var response = answer2.getAttribute("data-value");
+    var rightAnswer = questions[currentIndex].correctAnswer;
+    if(response === rightAnswer){
+        currentIndex++;
+        correctAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;
+    }
+    else{
+        secondsLeft -= 10;
+        timeDisplay.textContent = "Time: " + secondsLeft;
+        currentIndex++;
+        incorrectAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;
+    }
+}
+function checkAnswer3(){
+    var response = answer3.getAttribute("data-value");
+    var rightAnswer = questions[currentIndex].correctAnswer;
+    if(response === rightAnswer){
+        currentIndex++;
+        correctAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;
+    }
+    else{
+        secondsLeft -= 10;
+        timeDisplay.textContent = "Time: " + secondsLeft;
+        currentIndex++;
+        incorrectAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;;
+    }
+}
+function checkAnswer4(){
+    var response = answer4.getAttribute("data-value");
+    var rightAnswer = questions[currentIndex].correctAnswer;
+    if(response === rightAnswer){
+        currentIndex++;
+        correctAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;
+    }
+    else{
+        secondsLeft -= 10;
+        timeDisplay.textContent = "Time: " + secondsLeft;
+        currentIndex++;
+        incorrectAnswers++;
+        question.textContent = questions[currentIndex].questionText;
+        answer1.textContent = questions[currentIndex].answer1;
+        answer2.textContent = questions[currentIndex].answer2;
+        answer3.textContent = questions[currentIndex].answer3;
+        answer4.textContent = questions[currentIndex].answer4;;
+    }
+}
+answer1.addEventListener("click", checkAnswer1);
+answer2.addEventListener("click", checkAnswer2);
+answer3.addEventListener("click", checkAnswer3);
+answer4.addEventListener("click", checkAnswer4);
